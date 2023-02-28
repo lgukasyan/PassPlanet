343735cb8fb05ec7db8ca350c0d4f6147abd1e9e
Levon Gukasyan <gh.levon@outlook.com>
2023-02-28 15:23:09 +0100

    Added new table to store passwords

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
+-- Active: 1677423790558@@127.0.0.1@5432@passplanet
 /* User Table */
 create table users (
   user_id serial primary key,
@@ -7,5 +8,12 @@ create table users (
   password char(60)
 );
 
-
-
+create table passwords (
+  password_id serial primary key,
+  user_id serial references users(user_id) not null,
+  title varchar(50) not null,
+  url varchar(255),
+  icon_base64data text,
+  description varchar(100),
+  password char(60) not null
+);
