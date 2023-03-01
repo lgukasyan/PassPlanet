1bcdd3def3d0ef9fbc06fd1ddde70e3f735dbf33
Levon Gukasyan <gh.levon@outlook.com>
2023-03-01 20:00:22 +0100

    User table key added

--- db.sql
+++ db.sql
@@ -1,10 +1,12 @@
 -- Active: 1677423790558@@127.0.0.1@5432@passplanet
 /* User Table */
+
 create table users (
   user_id serial primary key,
   name varchar(255) not null,
   lastname varchar(255) not null,
   email varchar(255) not null,
+  key char(60) not null,
   password char(60)
 );
 
